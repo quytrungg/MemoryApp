41fdda10913c940e3107afa7b7ca867c101502ad
Albert <73238186+quytrungg@users.noreply.github.com>
2023-02-21 22:37:40 +0700

    Generate memory list and memory detail view

--- base/models.py
+++ base/models.py
@@ -11,7 +11,7 @@ class Memory(models.Model):
     create = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return self.title
+        return self.memory_name
     
     class Meta:
         ordering = ['complete']
--- base/views.py
+++ base/views.py
@@ -1,4 +1,28 @@
 from django.shortcuts import render
-
+from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
 # Create your views here.
 
+from .models import Memory
+
+class MemoryList(ListView):
+    model = Memory
+    context_object_name = 'memories'
+
+    # def get_context_data(self, **kwargs):
+    #     context = super().get_context_data(**kwargs)
+    #     context['memories'] = context['memories'].filter(user=self.request.user)
+    #     context['count'] = context['memories'].filter(complete=False).count()
+
+    #     search_input = self.request.GET.get('search-area') or ''
+    #     if search_input:
+    #         context['memories'] = context['memories'].filter(title__icontains=search_input)
+        
+    #     context['search_input'] = search_input
+
+    #     return context
+
+class MemoryDetail(DetailView):
+    model = Memory
+    context_object_name = 'memory'
+    template_name = 'base/memory.html'
