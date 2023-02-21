667246555fc46490a128eea105221e3455b4d50f
Albert <73238186+quytrungg@users.noreply.github.com>
2023-02-21 23:28:52 +0700

    Build create memory view and update memory view

--- base/views.py
+++ base/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
+from django.views.generic.edit import CreateView, UpdateView
+from django.urls import reverse_lazy
 # Create your views here.
 
 from .models import Memory
@@ -13,3 +15,13 @@ class MemoryDetail(DetailView):
     model = Memory
     context_object_name = 'memory'
     template_name = 'base/memory.html'
+
+class MemoryCreate(CreateView):
+    model = Memory
+    fields = ['place', 'memory_name', 'description', 'complete']
+    success_url = reverse_lazy('memories')
+
+class MemoryUpdate(UpdateView):
+    model = Memory
+    fields = ['place', 'memory_name', 'description', 'complete']
+    success_url = reverse_lazy('memories')
