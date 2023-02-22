2243995feb8c0d09f3ba850e3a357a9001d72c5d
Albert <73238186+quytrungg@users.noreply.github.com>
2023-02-22 10:40:21 +0700

    Build login view and logout view route

--- base/views.py
+++ base/views.py
@@ -4,10 +4,19 @@ from django.views.generic.detail import DetailView
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.urls import reverse_lazy
 
+from django.contrib.auth.views import LoginView
 from django.contrib.auth.mixins import LoginRequiredMixin
 # Create your views here.
 
 from .models import Memory
+
+class CustomLoginView(LoginView):
+    template_name = 'base/login.html'
+    fields = '__all__'
+    redirect_authenticated_user = True
+
+    def get_success_url(self):
+        return reverse_lazy('memories')
 
 class MemoryList(ListView):
     model = Memory
